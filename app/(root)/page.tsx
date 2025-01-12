f1166ed66434c732b4dee4e90eb38e7ab1f7c83d
powiedl <159603185+powiedl@users.noreply.github.com>
2025-01-12 13:01:35 +0100

    comment out delay function, as it is not used right now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import ProductList from '@/components/shared/product/product-list';
 import { getLatestProducts } from '@/lib/actions/products.actions';
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+//const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
   //  await delay(2000);
