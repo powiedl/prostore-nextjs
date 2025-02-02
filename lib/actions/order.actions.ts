1fe2205f0a6421389f810cb5262dd25b0af43f05
powiedl <159603185+powiedl@users.noreply.github.com>
2025-02-02 08:23:21 +0100

    added stripe webhook

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -187,7 +187,7 @@ export async function approvePayPalOrder(
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
